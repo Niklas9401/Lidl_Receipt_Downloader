97015f69807cde7391f4f212b206b6feb6e479f6
Niklas Weiblen <niklas.weiblen@outlook.de>
2023-12-11 22:28:30 +0100

    removed comments

--- extract.ts
+++ extract.ts
@@ -1,14 +1,10 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write
 
-const harJson = await Deno.readTextFile("receipts.json");
-const har = JSON.parse(harJson);
+const json = await Deno.readTextFile("receipts.json");
+const parsed_json = JSON.parse(json);
 
-// const resContentJson = har.log.entries[2].response.content.text;
-// const resContent = JSON.parse(resContentJson);
-// const html = har[0].ticket.htmlPrintedReceipt;
-
-for (let index = 0; index < har.length; index++) {
-    const html = har[index].ticket.htmlPrintedReceipt;
+for (let index = 0; index < parsed_json.length; index++) {
+    const html = parsed_json[index].ticket.htmlPrintedReceipt;
     await Deno.writeTextFile(`extracted_receipts/receipt_${index}.html`, html);
 }
 
